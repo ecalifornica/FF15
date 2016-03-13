7163721880549e861cbf82df7e1139af9ef77cd0
Robert Queenin <queenin@gmail.com>
2016-03-13 00:09:23 -0800

    spaghetti code. have put together drafted players for each team.

--- drafted_rankings.py
+++ drafted_rankings.py
@@ -3,9 +3,9 @@ import pprint
 from pymongo import MongoClient
 import requests
 
-from ff15_config import (APP_KEY,
-                         AUTH_TOKEN,
-                         LEAGUE_ID)
+from config import (APP_KEY,
+                    AUTH_TOKEN,
+                    LEAGUE_ID)
 
 mc = MongoClient()
 db = mc.ff15
@@ -115,11 +115,6 @@ def create_multiplier_table():
             pass
 
 
-def get_player_names():
-    r = requests.get('http://api.fantasy.nfl.com/v1/players/stats')
-    print(db['apiv1_player_names'].insert(r.json()))
-
-
 def create_player_names_collection(player_id=None):
     '''one call to api v1'''
 
@@ -136,69 +131,134 @@ def player_names(player_id=None):
     result = db.player_name_and_id.find_one({'_id': str(player_id)})
     return result['name']
 
+'''
+results = db.player_name_and_id.find()
+for i in results:
+    print i
+'''
 
-def get_player_details(player_id):
-    payload['playerId'] = player_id
-    ff_api('/player/details', payload, 'player_details')
-
-
-def league_settings():
-    url = base_url + '/league/settings'
-    r = requests.get(url, params=payload)
-    db.league_settings.insert(r.json())
-
-
-def matchups():
-    '''league/matchups'''
-    for week in range(1, 17):
-        payload['week'] = week
-        r = requests.get(base_url + '/league/matchups', params=payload)
-        pprint.pprint(r.json()['games'])
-        print(db.matchups.insert(r.json()))
-
-
-def player_stats():
-    '''players/weekstats'''
-    payload['season'] = '2015'
-    for week in range(1, 17):
-        payload['week'] = week
-        r = requests.get(base_url + '/players/weekstats', params=payload)
-        print(db.player_stats.insert(r.json()))
+def player_id_from_name(player_name=None):
+    import re
+    results = db.player_name_and_id.find({'name': re.compile(player_name, re.IGNORECASE)})
+    dbresult = list(results)
+    if len(dbresult) == 0:
+        print('No matches, calling player details API endpoint')
+        player_id = raw_input('Player ID: ')
+        payload['playerId'] = player_id
+        r = requests.get(base_url + '/player/details', params=payload)
+        player_name = r.json()['games']['102015']['players'][str(player_id)]['name']
+        return {u'_id': player_id, u'name': player_name}
+    elif len(dbresult) > 20:
+        print('More than 20 matches')
+        raise
+    for player in enumerate(dbresult, start=1):
+        print player[0], player[1]['name'], player[1]
+    user_input = raw_input('Choose player: ')
+    player_choice = dbresult[int(user_input) - 1]
+    return player_choice
 
 
-def league_transactions():
-    payload['count'] = '1000'
-    for feed_type in ['transactions', 'lmChanges']:
-        payload['feedType'] = feed_type
-        r = requests.get(base_url + '/league/feed', params=payload)
-        db.barf.insert(r.json())
+def draft_order():
+    '''
+    Hack
+    '''
+    #db.sunday_challenge_teams.drop()
+    previous_teams = list(db.sunday_challenge_teams.find())
+    pprint.pprint(previous_teams)
+    try:
+        for prev_team in previous_teams:
+            print prev_team['owner'], prev_team['draft position']
+            for player in enumerate(prev_team['drafted players']):
+                print player[0] + 1, player[1]
+        draft_position = int(raw_input('Draft position: '))
+        roster_position = int(raw_input('Start at roster position: '))
+        try:
+            team = previous_teams[draft_position - 1]
+        except:
+            owner = raw_input('Owner: ')
+            team = {'owner': owner, 'draft position': draft_position, 'drafted players': []}
+
+        for position in range(roster_position, 19):
+            player_query = raw_input('Partial player name, case insensitive: ')
+            player_choice = player_id_from_name(player_query)
+            print 'You selected {}, draft position {}.'.format(player_choice['name'], position)
+            team['drafted players'].append(player_choice)
+    except:
+        print team['owner']
+        for player in team['drafted players']:
+            print player
+    finally:
+        if raw_input('Insert into database? ') in ['y', 'yes']:
+            try:
+                print(db['sunday_challenge_teams'].replace_one({'_id': team['_id']}, team, upsert=True))
+            except:
+                print(db['sunday_challenge_teams'].insert(team))
+    
+
+class FFApi(object):
+    '''
+    '''
+    def __init__(self):
+        pass
+
+    def get_player_names():
+        r = requests.get('http://api.fantasy.nfl.com/v1/players/stats')
+        print(db['apiv1_player_names'].insert(r.json()))
+
+    def player_stats():
+        '''players/weekstats'''
+        payload['season'] = '2015'
+        for week in range(1, 17):
+            payload['week'] = week
+            r = requests.get(base_url + '/players/weekstats', params=payload)
+            print(db.player_stats.insert(r.json()))
+
+    def league_transactions():
+        payload['count'] = '1000'
+        for feed_type in ['transactions', 'lmChanges']:
+            payload['feedType'] = feed_type
+            r = requests.get(base_url + '/league/feed', params=payload)
+            db.barf.insert(r.json())
+
+    def stat_lookup_table():
+        '''game/stats'''
+        r = requests.get(base_url + '/game/stats', params=payload)
+        print(db.stat_lookup_table.insert(r.json()))
+
+    def game_settings():
+        '''?'''
+        r = requests.get(base_url + '/game/settings', params=payload)
+        db.game_settings.insert(r.json())
+
+    def roster_slots():
+        endpoint = '/game/rosterslots'
+        r = requests.get(base_url + endpoint, params=payload)
+        print(db.roster_slots.insert(r.json()))
+
+    def ff_api(endpoint, payload, col_name):
+        r = requests.get(base_url + endpoint, params=payload)
+        print(db[col_name].insert(r.json()))
+
+    def get_player_details(self, player_id):
+        payload['playerId'] = player_id
+        self.ff_api('/player/details', payload, 'player_details')
+
+    def league_settings():
+        url = base_url + '/league/settings'
+        r = requests.get(url, params=payload)
+        db.league_settings.insert(r.json())
+
+    def matchups():
+        '''league/matchups'''
+        for week in range(1, 17):
+            payload['week'] = week
+            r = requests.get(base_url + '/league/matchups', params=payload)
+            pprint.pprint(r.json()['games'])
+            print(db.matchups.insert(r.json()))
 
 
-def stat_lookup_table():
-    '''game/stats'''
-    r = requests.get(base_url + '/game/stats', params=payload)
-    print(db.stat_lookup_table.insert(r.json()))
-
-
-def game_settings():
-    '''?'''
-    r = requests.get(base_url + '/game/settings', params=payload)
-    db.game_settings.insert(r.json())
-
-
-def roster_slots():
-    endpoint = '/game/rosterslots'
-    r = requests.get(base_url + endpoint, params=payload)
-    print(db.roster_slots.insert(r.json()))
-
-
-def ff_api(endpoint, payload, col_name):
-    r = requests.get(base_url + endpoint, params=payload)
-    print(db[col_name].insert(r.json()))
-
-
-def test_team_score(week):
-    scores = {'team_2':
+def test_team_score(team=None, week=None):
+    scores = {2:
               {1: 143.34,
                2: 114.24,
                3: 142.68,
@@ -216,14 +276,15 @@ def test_team_score(week):
                15: 164.90,
                16: 139.44}
               }
-    return scores[week]
+    return scores[team][week]
 
 
 def main():
-    # print(db.collection_names())
+    print(db.collection_names())
+    
     for week in range(1, 17):
         team_score = team_score_for_week(team=2, week=week)
-        nfl_score = test_team_score(week)
+        nfl_score = test_team_score(week=week, team=2)
         # Hack
         score_delta = round(team_score - nfl_score, 2)
         print('Week {} score: {}, NFL: {}, delta: {}'.format(week,
